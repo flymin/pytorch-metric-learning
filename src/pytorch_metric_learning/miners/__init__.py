a84bbd71c81d0904994250faeb5d6cc5b46cb7f7
flymin <905370712@qq.com>
2021-02-23 19:03:50 +0800

    add MyTripletMarginMiner

--- src/pytorch_metric_learning/miners/__init__.py
+++ src/pytorch_metric_learning/miners/__init__.py
@@ -8,5 +8,5 @@ from .hdc_miner import HDCMiner
 from .maximum_loss_miner import MaximumLossMiner
 from .multi_similarity_miner import MultiSimilarityMiner
 from .pair_margin_miner import PairMarginMiner
-from .triplet_margin_miner import TripletMarginMiner
+from .triplet_margin_miner import TripletMarginMiner, MyTripletMarginMiner
 from .uniform_histogram_miner import UniformHistogramMiner
